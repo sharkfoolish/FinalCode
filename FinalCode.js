5dd1892e0b0eb2ca2cde19a0d1cdad6449985335
sharkfoolish <sharkfoolish@chsh.chc.edu.tw>
2022-04-24 16:05:45 +0800

    add record table

--- FinalCode.js
+++ FinalCode.js
@@ -9,15 +9,22 @@ const FinalCode = {
     judge : function(inputAnswer){
         if (this.password == inputAnswer-0) {
             alert("你花了" + this.number + "次，猜對答案" + this.password);
+            this.rederRecord(inputAnswer, '正確');
         }
         else if (this.password > inputAnswer-0) {
+            this.rederRecord(inputAnswer, '錯誤');
             this.Min = inputAnswer;
             this.number+=1;
         }
         else if (this.password < inputAnswer-0){
+            this.rederRecord(inputAnswer, '錯誤');
             this.Max = inputAnswer;
             this.number+=1;
         }
-    }
+    },
+
+    rederRecord : function(inputAnswer, status){
+        this.record += '<tr><th scope="row">'+ this.number + '</th><td>' + new Date().toLocaleDateString() + ' ' +new Date().toLocaleTimeString() + '</td><td>'+ inputAnswer +'</td><td>'+ status +'</td></tr>';
+    },
 
 };
